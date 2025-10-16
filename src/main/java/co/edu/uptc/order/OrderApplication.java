ff16b2e14853e9c6d531976a6628efeef63d71c4
jagd3 <jagd334@gmail.com>
2025-10-16 09:27:30 -0500

    first commit

---
+++ src/main/java/co/edu/uptc/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptx.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
